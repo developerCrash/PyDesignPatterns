74043bd3fac5493e08bcc5e1350ca571735c2606
Anil Kumar G <35560797+developerCrash@users.noreply.github.com>
2024-07-24 18:34:21 +0530

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,14 +1,4 @@
-from Factory.Flutter import *
-from SupportedPlatform import *
 
-
-class Client:
-    if __name__ == '__main__':
-        f = Flutter("IOS")
-        ui = f.createUIFac()
-        button = ui.createButton()
-        scroll = ui.createScroll()
-        dropdown = ui.createDropDown()
 
 
 
